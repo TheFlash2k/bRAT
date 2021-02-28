80e5b1b3f54cae444fa3e46f4fcce92cbc99e233
TheFlash2k <alitaqi2000@gmail.com>
2021-03-01 04:01:57 +0500

    Fixed bugs

--- compile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo Compiling Victim.cpp
-g++ victim.cpp -o victim -w
-echo Compilation Complete
-sleep 1
-echo Compiling Attacker.cpp
-g++ attacker.cpp -o attacker -w
-echo Compilation Complete
